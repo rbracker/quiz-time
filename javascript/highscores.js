b3701bba6f4a2ac9e89f63b646580a5abee60512
Ranee Bracker <rbracker@asu.edu>
2024-01-03 22:22:27 -0700

    changed display and fixed bugs

--- javascript/highscores.js
+++ javascript/highscores.js
@@ -1,23 +1,59 @@
 function saveHighscore() {
-    const initials = document.getElementById('submit').value.trim();
-    if (initials !== '') {
-      const scoreData = {
-        initials: initials,
-        score: score,
-      };
-  
-      const storedHighscores = localStorage.getItem('highscores');
-      const parsedHighscores = JSON.parse(storedHighscores) || [];
-  
-      parsedHighscores.push(scoreData);
-  
-      parsedHighscores.sort((a, b) => b.score - a.score);
+  console.log('saveHighscore function called');
+  const initials = document.getElementById('submit').value.trim();
+  console.log('Initials:', initials);
 
-      localStorage.setItem('highscores', JSON.stringify(parsedHighscores));
+  if (initials !== '') {
+    const scoreData = {
+      initials: initials,
+      score: score, 
+    };
 
-      window.location.href = './highscores.html';
-      
-    } else {
-      alert('Please enter your initials.');
-    }
+    const storedHighscores = localStorage.getItem('highscores');
+    const parsedHighscores = JSON.parse(storedHighscores) || [];
+
+
+    parsedHighscores.push(scoreData);
+
+    parsedHighscores.sort((a, b) => b.score - a.score);
+
+    localStorage.setItem('highscores', JSON.stringify(parsedHighscores));
+
+    window.location.href = './highscores.html';
+    
+  } else {
+    alert('Please enter your initials.');
   }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const highscores = getHighscores();
+
+  displayHighscores(highscores);
+
+  const backToQuizButton = document.getElementById('back-to-quiz-btn');
+  if (backToQuizButton) {
+    backToQuizButton.addEventListener('click', function () {
+      // Redirect to the quiz page
+      window.location.href = './index.html'; // Adjust the path based on your file structure
+    });
+  }
+});
+
+function getHighscores() {
+  const storedHighscores = localStorage.getItem('highscores');
+  return JSON.parse(storedHighscores) || [];
+}
+
+function displayHighscores(highscores) {
+  const highscoresList = document.getElementById('list-of-highscores');
+
+  highscoresList.innerHTML = '';
+
+  highscores.forEach(function (scoreData) {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${scoreData.initials}: ${scoreData.score}`;
+    highscoresList.appendChild(listItem);
+  });
+}
+submitButton.addEventListener('click', saveHighscore);
